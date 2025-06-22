5cbec4e7345575bc54dd3b24ee427e15a369e3f0
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2025-06-22 10:16:50 +0200

    improved mouse movement

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@ mod camera;
 
 extern crate kiss3d;
 
-use kiss3d::camera::Camera;
 use kiss3d::event::{Action, Key, WindowEvent};
-use kiss3d::nalgebra::{Vector3, UnitQuaternion, Translation, Point3, Translation3};
+use kiss3d::nalgebra::{Vector3, Translation, Point3, Translation3};
 use kiss3d::window::Window;
 use kiss3d::light::Light;
 use crate::camera::MyCamera;
 
 fn main() {
-    let mut window = Window::new("Kiss3d: cube");
+    let mut window = Window::new_with_size("VoidFactory", 1000, 800);
     let mut c = window.add_cube(1.0, 1.0, 1.0);
     c.append_translation(&Translation::<f32,3>::new(0.0, 0.0, 3.0));
     c.set_color(1.0, 0.0, 0.0);
@@ -29,10 +28,12 @@ fn main() {
     
     window.set_cursor_position(window.size()[0] as f64 / 2.0, window.size()[1] as f64 / 2.0);
     window.set_cursor_grab(true);       // Lock cursor to window
-    window.hide_cursor(true);   // Hide cursor
+    window.hide_cursor(true);      // Hide cursor
 
     camera.set_yaw_pitch(yaw, pitch);
 
+    let mut prev_cursor_pos = (window.size()[0] as f64 / 2.0, window.size()[1] as f64 / 2.0);
+    
     while window.render_with_camera(&mut camera) {
         // check for user input to move camera
         let mut movement: Vector3<f32> = Vector3::zeros();
@@ -68,33 +69,46 @@ fn main() {
             camera = camera.translate(&Translation3::from(delta));
         }
         
+        let curr_cursor_pos = window.cursor_pos().unwrap_or(prev_cursor_pos);
+        if curr_cursor_pos != prev_cursor_pos {
+
+            // Calculate the delta from the center of the window
+            let delta_x = curr_cursor_pos.0 - prev_cursor_pos.0;
+            let delta_y = curr_cursor_pos.1 - prev_cursor_pos.1;
+            
+            if delta_x.abs() < 500.0 && delta_y.abs() < 200.0 {
+                // sometimes the cursor can jump, so we ignore large deltas
+
+                prev_cursor_pos = curr_cursor_pos;
+
+                // Update camera rotation based on cursor movement
+                yaw += delta_x as f32 * 0.001; // Adjust sensitivity as needed
+                pitch += delta_y as f32 * 0.001; // Adjust sensitivity as needed
+
+                // Clamp pitch to avoid gimbal lock
+                if pitch > std::f32::consts::PI - 0.01 {
+                    pitch = std::f32::consts::PI - 0.01;
+                } else if pitch < 0.01 {
+                    pitch = 0.01;
+                }
+                camera.set_yaw_pitch(yaw, pitch);
+            }
+
+            if curr_cursor_pos.0 < 10.0 || curr_cursor_pos.0 > window.size()[0] as f64 - 10.0 ||
+                curr_cursor_pos.1 < 10.0 || curr_cursor_pos.1 > window.size()[1] as f64 - 10.0 {
+                // Reset cursor position to the center of the window
+                let window_middle_x = window.size()[0] as f64 / 2.0;
+                let window_middle_y = window.size()[1] as f64 / 2.0;
+                window.set_cursor_position(window_middle_x, window_middle_y);
+                prev_cursor_pos = (window_middle_x, window_middle_y);
+            }
+        }
+        
         for mut event in window.events().iter() {
             match event.value {
                 WindowEvent::Key(Key::Escape, Action::Press, _) => {
                     window.close();
                     event.inhibited = true;
-                }
-                WindowEvent::CursorPos(x, y, _) => {
-                    let window_middle_x = window.size()[0] as f64 / 2.0;
-                    let window_middle_y = window.size()[1] as f64 / 2.0;
-
-                    // Calculate the delta from the center of the window
-                    let delta_x = x - window_middle_x;
-                    let delta_y = y - window_middle_y;
-
-                    // Update camera rotation based on cursor movement
-                    yaw += delta_x as f32 * 0.001; // Adjust sensitivity as needed
-                    pitch += delta_y as f32 * 0.001; // Adjust sensitivity as needed
-
-                    // Clamp pitch to avoid gimbal lock
-                    if pitch > std::f32::consts::PI - 0.01 {
-                        pitch = std::f32::consts::PI - 0.01;
-                    } else if pitch < 0.01 {
-                        pitch = 0.01;
-                    }
-                    camera.set_yaw_pitch(yaw, pitch);
-
-                    window.set_cursor_position(window_middle_x, window_middle_y);
                 }
                 _ => {}
             }
